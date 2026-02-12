367dfda6bc68316d317de294debc7f2cc3f7104b
lhagvaa0802-lab <lhagvaa0802@gmail.com>
2026-02-12 06:24:55 -0500

    draft

--- src/lib/apiCredit.ts
+++ src/lib/apiCredit.ts
@@ -1,7 +1,7 @@
 import { MovieCredits } from "./types";
 import { SimilarMovie } from "./types";
 const token =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MThkODk5NDdiODJjMzJmMGJmMTM2ZTdlN2NkYzg1NyIsIm5iZiI6MTc3MDYwODMyMC4yNjEsInN1YiI6IjY5ODk1NmMwZGRhMDQ2OWIwODVjYWViZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JmBMnwPwo4W58j6BY3FNxYKbUgfB3eKPXvW9D2XdU1s";
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1Yzg1NyIsIm5iZiI6MTc3MDYwODMyMC4yNjEsInN1YiI6IjY5ODk1NmMwZGRhMDQ2OWIwODVjYWViZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JmBMnwPwo4W58j6BY3FNxYKbUgfB3eKPXvW9D2XdU1s";
 const option = {
   method: "GET",
   headers: {
